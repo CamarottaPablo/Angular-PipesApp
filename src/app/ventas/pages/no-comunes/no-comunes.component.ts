a18c569131e7083a4949e09b9370d735f66493ce
CamarottaPablo <83800798+CamarottaPablo@users.noreply.github.com>
2021-11-21 17:17:50 -0300

    Slice Pipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -8,12 +8,30 @@ import { Component } from '@angular/core';
 })
 export class NoComunesComponent {
 
+  //i18nSelect
   nombre : string = 'Pablo';
   genero : string = 'masculino';
-
   invitacionMapa = {
     'masculino': 'invitarlo',
     'femenino': 'invitarla'
+  }
+
+  //i18nPlural
+  clientes: string[] = ['Maria', 'Pedro', 'Juan', 'Jorge', 'Nina'];
+  clientesMapa = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 clientes esperando.',
+    'other': 'tenemos # clientes esperando.'
+  }
+
+  cambiarCliente() {
+    this.nombre = 'Nina',
+    this.genero = 'femenino'
+  }
+
+  borrarCliente() {
+    this.clientes.pop();  
   }
 
 }
